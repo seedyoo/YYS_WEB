44e49022fc73dfb60bf257324f6c7cdf64469715
seeyoo <lukdown10@gmail.com>
2023-05-18 14:52:29 +0900

    ,.,.

---
+++ Day09/01_slide/slick/js/script.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    
+    // 슬라이드 시작!
+    $('.slide-container').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+    })
+
+})
